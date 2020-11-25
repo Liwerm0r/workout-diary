05ff17aec37c3cc7b1a5a7f9c33b8942aaffe4f3
Rafał Ostrowidzki <r.ostrowidzki@gmail.com>
2020-11-25 19:53:20 +0100

    initial commit

---
+++ src/main/java/pl/ostrowidzki/workoutdiary/WorkoutDiaryApplication.java
@@ -0,0 +1,13 @@
+package pl.ostrowidzki.workoutdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ostrowidzki/workoutdiary/WorkoutDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ostrowidzki.workoutdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
