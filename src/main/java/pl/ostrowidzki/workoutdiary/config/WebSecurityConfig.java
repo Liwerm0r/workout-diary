0b44a3328a4f91492b96ec153b7afe765a0ce08d
Rafał Ostrowidzki <r.ostrowidzki@gmail.com>
2020-11-26 15:32:14 +0100

    remove in memory auth, add two predefined accounts to db

---
+++ src/main/java/pl/ostrowidzki/workoutdiary/config/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package pl.ostrowidzki.workoutdiary.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import pl.ostrowidzki.workoutdiary.dao.UserRepository;
+import pl.ostrowidzki.workoutdiary.entity.User;
+import pl.ostrowidzki.workoutdiary.service.UserDetailsServiceImpl;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsServiceImpl userDetailsService;
+    private UserRepository userRepository;
+
+    @Autowired
+    public WebSecurityConfig(UserDetailsServiceImpl userDetailsService, UserRepository userRepository) {
+        this.userDetailsService = userDetailsService;
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/test").hasRole("ADMIN")
+                .and()
+                .formLogin().permitAll()
+                .and().logout().permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initialize() {
+        User userUser = new User("user", passwordEncoder().encode("userpassword"), "ROLE_USER");
+        User userAdmin = new User("admin", passwordEncoder().encode("adminpassword"), "ROLE_ADMIN");
+        userRepository.save(userUser);
+        userRepository.save(userAdmin);
+    }
+}
